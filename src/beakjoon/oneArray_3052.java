da64e06fa988cefdba5614c23555d05022fc049c
DESKTOP-F89735T\이여름 <yl9517@naver.com>
2021-06-29 23:42:05 +0900

    1차원 배열 - 10개의 값을 입력받아 42로 나눴을때 서로다른 나머지 값 갯수

---
+++ src/beakjoon/oneArray_3052.java
@@ -0,0 +1,37 @@
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class oneArray_3052 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] input = new int[10];
+		List<Integer> arr = new ArrayList<Integer>();
+		
+		for(int i=0; i<input.length; i++) { //숫자 입력받기
+			input[i] = Integer.parseInt(br.readLine());
+		}
+		
+		
+		for(int i=0; i<input.length; i++) { //배열만큼 돌려서
+			//42로 나눈 숫자가 arr배열에 있지 않으면 추가
+			int left = input[i]%42;
+			if(!arr.contains(left)) 
+				arr.add(left);
+		}
+		
+		//arr갯수 출력
+		System.out.println(arr.size());
+		
+	}
+
+}
