4bc177cefbfb65ea0a9596a2d120d846c8edc47d
DESKTOP-F89735T\이여름 <yl9517@naver.com>
2021-10-29 00:12:30 +0900

    정렬 추가

--- Algorithm/src/Programmers/array_k.java
+++ Algorithm/src/Programmers/array_k.java
@@ -10,6 +10,7 @@ public class array_k {
 		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
 		
 		int[] ans = solution(array, commands);
+		
 		System.out.println("답");
 		for(int i=0; i<ans.length; i++) {
 			System.out.print(ans[i]+" ");
@@ -17,15 +18,30 @@ public class array_k {
 	}
 
 	    public static int[] solution(int[] array, int[][] commands) {
-	    	int[] answer = new int[3];
-	    	
+	    	int[] answer = new int[commands.length];
 	    	for(int i=0; i<commands.length; i++) {
 	    		int f = commands[i][0];
 	    		int l = commands[i][1];
 	    		int k = commands[i][2];
 	    		
-	    		int[] twoArray = Arrays.copyOfRange(array, f-1, l); //배열 자르기
-
+	    		int[] twoArray = Arrays.copyOfRange(array, f-1, l); //배열 자르기	    		
+	    		int tmp;
+	    		
+	    		//정렬1
+	    		//Arrays.sort(temp); 
+	    		
+	    		//정렬2
+	    		for(int j=0; j<twoArray.length-1; j++) {
+	    			int min = j;
+	    			for(int h=j+1; h<twoArray.length; h++) {
+	    				if(twoArray[h] < twoArray[min]) 
+	    					min = h;	
+	    			}
+	    				
+	    				tmp = twoArray[j];
+	    				twoArray[j] = twoArray[min];
+	    				twoArray[min] = tmp;
+	    		}
 	    		
 	    		answer[i] = twoArray[k-1];
 	    		
